386b1ab299ac3d53e3bbfd3c553254c998f246fa
Jack Sloan <jbsloan10@gmail.com>
2021-08-31 19:24:00 -0500

    add cdk stepper module to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlyMaterialModule } from '@ngx-formly/material';
+import { CdkStepperModule } from '@angular/cdk/stepper';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     FormlyModule.forRoot({ extras: { lazyRender: true } }),
-    FormlyMaterialModule
+    FormlyMaterialModule,
+    CdkStepperModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
